e0533d87cf1979b56c08e7a0dc3e05e714aa9b31
Mark Linn <marktlinn@gmail.com>
2022-09-09 14:50:30 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+// import Confetti from "react-confetti"
+// import Die from "./components/"
+
+function App() {
+  return (
+    <div>
+    <h1>Hello World</h1>
+    </div>
+  )
+}
+
+
+export default App
