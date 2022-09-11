4571909bd70052296a27f5a75c040072ffd9835d
Mark Linn <marktlinn@gmail.com>
2022-09-11 15:52:23 +0200

    generating array of numbers for dice, button created to roll dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import React from "react"
 import Die from "./components/Die"
 
 function App() {
+  const [dice, setDieNum] = React.useState(allNewDice())
+
+// function to generate a random number between 1-6
+  function allNewDice (){
+    const nums =  []
+      for(let i = 0; i<10; i++){
+      nums.push(Math.floor(Math.random() * (6 - 1 + 1) + 1))
+    } 
+    return nums
+  }
+  function rollDice(){
+    setDieNum(allNewDice());
+  }
+
+
+  const dieSet = dice.map(elem=> <Die value={elem}/>)
+  console.log(dieSet)
   return (
     <main>
       <h1 className="titleText">Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className="dice-section">
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-        <Die num={1}/>
-
+      <div 
+        className="dice-section">
+          {dieSet}
       </div>
+      <button
+      onClick={rollDice}
+      >Roll Dice</button>
     </main>
 
   )
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function Die(props) {
-    const { num: value } = props;
+    const { value } = props;
     return (
         <div className="die">
             <span className="die-number">{value}</span>
