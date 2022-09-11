488b85a235a6543acccbcf914169bfaabc3762e7
Mark Linn <marktlinn@gmail.com>
2022-09-11 16:46:29 +0200

    selecting dice and updating dom without issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import React from "react"
 // import Confetti from "react-confetti"
 import Die from "./components/Die"
+import {nanoid} from 'nanoid'
 
 function App() {
   const [dice, setDieNum] = React.useState(allNewDice())
-
 // function to generate a random number between 1-6
   function allNewDice (){
     const nums =  []
       for(let i = 0; i<10; i++){
-      nums.push(Math.floor(Math.random() * (6 - 1 + 1) + 1))
+      nums.push({
+        value: Math.floor(Math.random() * (6 - 1 + 1) + 1), 
+        isHeld: false,
+        id: nanoid()
+      })
     } 
     return nums
   }
+  
+  function holdDie(dieId){
+    setDieNum(prev=> prev.map(die=> {
+      return die.id === dieId ? {...die, isHeld: !die.isHeld} : die; 
+    }))
+  }
+
   function rollDice(){
     setDieNum(allNewDice());
   }
 
-
-  const dieSet = dice.map(elem=> <Die value={elem}/>)
-  console.log(dieSet)
+console.log(dice)
+  const dieSet = dice.map(elem=> {
+  return <Die 
+  key={elem.id}
+  value={elem.value}
+  isHeld={elem.isHeld}
+  holdDie={()=>holdDie(elem.id)}
+  />
+})
   return (
     <main>
       <h1 className="titleText">Tenzies</h1>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 
 export default function Die(props) {
-    const { value } = props;
+    const { value, isHeld, holdDie } = props;
     return (
-        <div className="die">
+        <div 
+            className={isHeld === true? 'die dieHeld': 'die'}
+            onClick={holdDie}
+            >
             <span className="die-number">{value}</span>
         </div>
     )
